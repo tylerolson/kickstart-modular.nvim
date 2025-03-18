ff1cbd2351f01f377a6866b24330d0e7023c65d3
Tyler Olson <tolson519@gmail.com>
2025-03-18 11:18:26 -0700

    add bufferline keymaps, indicators, and numbers

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -6,6 +6,10 @@ return {
     require('bufferline').setup {
       options = {
         diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count, level)
+          local icon = level:match 'error' and ' ' or ' '
+          return ' ' .. icon .. count
+        end,
         hover = {
           enabled = true,
           delay = 100,
@@ -14,13 +18,18 @@ return {
         offsets = {
           {
             filetype = 'neo-tree',
-            text = 'Neo-tree',
+            text = 'neo-tree',
             highlight = 'Directory',
-            text_align = 'left',
-            separator = true, -- use a "true" to enable the default, or set your own character
+            text_align = 'center',
+            separator = false, -- this is stupid
           },
         },
+
+        numbers = 'ordinal',
       },
     }
+
+    vim.keymap.set('n', 'gb', '<Cmd>BufferLinePick<CR>', { silent = true, desc = 'Pick a buffer' })
+    vim.keymap.set('n', 'gB', '<Cmd>BufferLinePickClose<CR>', { silent = true, desc = 'Delete a buffer' })
   end,
 }
